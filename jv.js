50a83810bef10d0b99d1696e38870e3abdaf6088
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-05-15 00:49:41 +0300

    updates

--- jv.js
+++ jv.js
@@ -1,4 +1,13 @@
 
+ var productIndex = 1;
+  
+   function changeproductIndex(n)
+   {
+      alert("calling changeproductIndex()");
+      //productIndex=n;
+      //alert(productIndex);
+   }
+  
   
     function load() {
                      var mydata = JSON.parse(data);
@@ -11,18 +20,24 @@
                      {
                           /* div.innerHTML = div.innerHTML + "<p class='inner' id="+i+">"+ mydata[i].name +"</p>" + "<br>"; */
                         
+                        
+                        
                         var iDiv = document.createElement('div');
                              iDiv.id = 'kerah';
                              iDiv.className = 'col-md-4';
                         
+                            
                         
-                            var img = document.createElement("img");
+                        var img = document.createElement("img");
                             img.src = "product"+mydata[i].index+".jpg";
                             img.classList.add('center');
                         
-                             
-                             iDiv.appendChild(img);
-                             iDiv.innerHTML +=  mydata[i].name + mydata[i].index;
+                        var a = document.createElement("a");
+                              a.href = "product.html"
+                              //a.onclick= changeproductIndex(mydata[i].index)
+                             iDiv.appendChild(a);
+                             a.appendChild(img);
+                             iDiv.innerHTML +=  mydata[i].name;
                              //iDiv.innerHTML =  mydata[i].name + " " + mydata[i].index;
                            
                              
